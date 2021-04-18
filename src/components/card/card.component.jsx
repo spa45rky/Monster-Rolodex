f84c8a5bcd0573486b923af09aca6b73d7c6465f
Spa45rky <spa45rky@gmail.com>
2021-04-18 14:29:32 +0500

    added search box

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './card.styles.css'
 
-export const Card = (props) => (
+export const Card = props => (
     <div className='card-container'>
         <img alt='monster' src={`https://robohash.org/${props.monster.id + 10}?set=set2&size=180x180`}/>
         <h2> {props.monster.name} </h2>
